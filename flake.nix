dba99c23ca4105c81d5f8374876fb053574c71e2
aster <137767097+aster-void@users.noreply.github.com>
2025-04-20 18:58:39 +0900

    init shell

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Shortcut game";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    bunnix.url = "github:aster-void/bunnix";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    bunnix,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          bunnix.packages.${system}.v1_2_10
+        ];
+      };
+    });
+}
