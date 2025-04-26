131d220909c0309867a47209b1ce14b9f20697a8
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-04-26 14:17:24 +0900

    create stage1

--- src/grid.ts
+++ src/grid.ts
@@ -1,38 +1,17 @@
+import createStage1 from "./stages/createStage1.ts";
+
 export enum Block {
   air = "air",
   block = "block",
   movable = "movable",
 }
 
-export const grid: Block[][] = [];
-export const gridX = 17;
-export const gridY = 16;
-export function getRandom() {
-  switch (Math.floor(Math.random() * 3)) {
-    case 0:
-      return Block.air;
-    case 1:
-      return Block.block;
-    case 2:
-      return Block.movable;
-    default:
-      throw new Error("oops!");
-  }
-}
+export const gridX = 18;
+export const gridY = 6;
+export const grid: Block[][] = createStage1(gridX);
 export function getBlock(x: number, y: number) {
   return grid[y][x];
 }
 export function setBlock(x: number, y: number, block: Block) {
   grid[y][x] = block;
 }
-
-export function shuffleGrid() {
-  grid.length = 0;
-  for (let y = 0; y < gridY; y++) {
-    grid.push([]);
-    for (let x = 0; x < gridX; x++) {
-      grid[y].push(getRandom());
-    }
-  }
-}
-shuffleGrid();
