467c14d642b2c8f4b907a31caf609efa7a0d83eb
aster <137767097+aster-void@users.noreply.github.com>
2025-04-26 17:22:22 +0900

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: ["es2022", "edge89", "firefox89", "chrome89", "safari15"],
+  },
   server: {
     port: 8080,
     open: true,
