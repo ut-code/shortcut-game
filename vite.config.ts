f8cafc9423df32c352f4cb754e3d008a6c975ae9
aster <137767097+aster-void@users.noreply.github.com>
2025-04-26 17:00:28 +0900

    Revert "init sveltekit for router"
    This reverts commit 206a3f41f8da92ab9addf7756a7106cb7d20df1f.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-
 export default defineConfig({
-  plugins: [sveltekit()],
   server: {
     port: 8080,
     open: true,
