7ff50d10571d25566fa24cbee12fb0670dc0afe8
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-04-26 11:49:20 +0900

    init character

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,31 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application, Assets, Container, Sprite } from "pixi.js";
 
 (async () => {
   // Create a new application
   const app = new Application();
 
   // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+  await app.init({ background: "white", resizeTo: window });
 
   // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 
+  const stageContainer = new Container();
+  app.stage.addChild(stageContainer);
+  const rockTexture = await Assets.load("/assets/block.png");
+
+  for (let i = 0; i < 10; i++) {
+    const rock = new Sprite(rockTexture);
+
+    rock.width = 30;
+    rock.height = 30;
+    rock.x = (i % 5) * 40;
+    rock.y = Math.floor(i / 5) * 40;
+    stageContainer.addChild(rock);
+  }
+
+  stageContainer.x = app.screen.width / 2;
+  stageContainer.y = app.screen.height / 2;
   // Load the bunny texture
   const texture = await Assets.load("/assets/bunny.png");
 
@@ -20,16 +36,20 @@ import { Application, Assets, Sprite } from "pixi.js";
   bunny.anchor.set(0.5);
 
   // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
+  bunny.position.set(app.screen.width /2 + 30, app.screen.height / 2 - 20);
 
   // Add the bunny to the stage
   app.stage.addChild(bunny);
 
+  document.addEventListener("keydown", (event) => {
+    if (event.ctrlKey && event.key === "b") {
+      bunny.x += 10;
+    }
+    if (bunny.x >= app.screen.width / 2 + 200) {
+      app.stage.removeChild(bunny);
+    }
+    return;
+  })
   // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
+
 })();
