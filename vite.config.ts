206a3f41f8da92ab9addf7756a7106cb7d20df1f
aster <137767097+aster-void@users.noreply.github.com>
2025-04-26 16:13:55 +0900

    init sveltekit for router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+
 export default defineConfig({
+  plugins: [sveltekit()],
   server: {
     port: 8080,
     open: true,
