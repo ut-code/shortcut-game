6121618a95ebc040b4611a99eb6e56b61b78758a
aster <137767097+aster-void@users.noreply.github.com>
2025-04-27 16:47:09 +0900

    use /game route for all games

--- routes/+page.svelte
+++ routes/+page.svelte
@@ -3,6 +3,6 @@
 
 <h1>Welcome!</h1>
 
-<a href="/stage-1">Stage 1</a>
-<a href="/stage-2">Stage 2</a>
-<a href="/stage-3">Stage 3</a>
+<a href="/game?stage=1">Stage 1</a>
+<a href="/game?stage=2">Stage 2</a>
+<a href="/game?stage=3">Stage 3</a>
---
+++ routes/game/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { goto } from "$app/navigation";
+import { page } from "$app/state";
+import { setup } from "@/main.ts";
+import * as grids from "@/stages";
+let container: HTMLElement | null = $state(null);
+
+const gridMap = new Map([
+  ["1", grids.grid1],
+  ["2", grids.grid2],
+  ["3", grids.grid3],
+]);
+
+$effect(() => {
+  const grid = gridMap.get(page.url.searchParams.get("stage") ?? "");
+  if (!grid) {
+    goto("/");
+    return;
+  }
+  if (container) {
+    setup(container, grids.grid1);
+  }
+});
+</script>
+
+<div id="app">
+  <div bind:this={container}></div>
+</div>
