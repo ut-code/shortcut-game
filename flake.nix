3b1ef4e158266bbb9f4f1aef8831e21edb2d21e7
aster <137767097+aster-void@users.noreply.github.com>
2025-04-27 16:13:55 +0900

    revive flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Shortcut game";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.bun
+          pkgs.nodejs-slim
+          pkgs.static-web-server
+        ];
+      };
+    });
+}
