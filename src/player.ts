c4df27904289ef230ef3a6eca75851999c6d822f
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-04-28 03:04:53 +0900

    プレイヤーが左右両方埋まっている場合など当たり判定のバグを修正&リファクタリング

--- src/player.ts
+++ src/player.ts
@@ -54,7 +54,7 @@ export class Player extends Sprite {
   }
   getCoords(cx: Context) {
     const x = Math.floor(this.x / cx.blockSize);
-    const y = Math.floor((this.y - 1) / cx.blockSize);
+    const y = Math.ceil(this.y / cx.blockSize) - 1;
     return { x, y };
   }
   createHighlight(cx: Context) {
@@ -141,63 +141,62 @@ export class Player extends Sprite {
     const isBlock = (x: number, y: number) =>
       getBlock(cx.grid, Math.floor(x), Math.floor(y)) !== Block.air &&
       getBlock(cx.grid, Math.floor(x), Math.floor(y)) !== undefined;
-    const isOutOfWorld = (x: number, y: number) =>
-      getBlock(cx.grid, Math.floor(x), Math.floor(y)) === undefined;
+    const isOutOfWorldLeft = (x: number) => x < 0;
+    const isOutOfWorldRight = (x: number) => x >= cx.gridX;
+    const isOutOfWorldBottom = (y: number) => y >= cx.gridY;
 
+    // next〜 は次フレームの座標、inner〜 は前フレームでかつ1px内側の座標
     const nextX = (this.x + this.vx * ticker.deltaTime) / cx.blockSize;
-    const bottomY = (this.y + this.vy * ticker.deltaTime) / cx.blockSize;
-    const topY = bottomY - c.playerHeight;
-    const leftX = nextX - c.playerWidth / 2;
-    const rightX = nextX + c.playerWidth / 2;
-
+    const nextBottomY = (this.y + this.vy * ticker.deltaTime) / cx.blockSize;
+    const nextTopY = nextBottomY - c.playerHeight;
+    const nextLeftX = nextX - c.playerWidth / 2;
+    const nextRightX = nextX + c.playerWidth / 2;
     const innerBottomY = (this.y - 1) / cx.blockSize;
     const innerTopY = (this.y + 1) / cx.blockSize - c.playerHeight;
     const innerLeftX = (this.x + 1) / cx.blockSize - c.playerWidth / 2;
     const innerRightX = (this.x - 1) / cx.blockSize + c.playerWidth / 2;
 
     // 天井
-    const hittingCeil = isBlock(innerLeftX, topY) || isBlock(innerRightX, topY);
-    if (this.vy < 0 && hittingCeil) {
-      this.y = Math.ceil(bottomY) * cx.blockSize;
-      this.vy = 0;
-      this.jumpingBegin = null;
-    }
-    this.x += this.vx * ticker.deltaTime;
-    this.y += this.vy * ticker.deltaTime;
-    this.vy += c.gravity * cx.blockSize * ticker.deltaTime;
-
+    const hittingCeil =
+      isBlock(innerLeftX, nextTopY) || isBlock(innerRightX, nextTopY);
     // プレイヤーの下がブロック
     // = プレイヤーの左下端がブロック or プレイヤーの右下端がブロック
     // 壁に触れている際にバグるのを避けるためx方向は±1
     this.onGround =
       this.vy >= 0 &&
-      (isBlock(innerLeftX, bottomY) || isBlock(innerRightX, bottomY));
-    if (this.onGround) {
+      (isBlock(innerLeftX, nextBottomY) || isBlock(innerRightX, nextBottomY));
+    if (hittingCeil && this.onGround) {
+      this.vy = 0;
+    } else if (hittingCeil) {
+      this.y = Math.ceil(nextBottomY) * cx.blockSize;
+      this.vy = 0;
+      this.jumpingBegin = null;
+    } else if (this.onGround) {
       // 自分の位置は衝突したブロックの上
-      if (!hittingCeil) {
-        this.y = Math.floor(bottomY) * cx.blockSize;
-      }
+      this.y = Math.floor(nextBottomY) * cx.blockSize;
       this.vy = 0;
     }
     // プレイヤーの右上端または右下端がブロック または右画面端
     // = プレイヤーの右上端がブロック or 右下端がブロック or プレイヤーの右側が世界の外 (世界は長方形のため、チェックは 1 回でいい)
     // 地面に触れている際にバグるのを避けるためy方向は-1
-    if (
-      isBlock(rightX, innerBottomY) ||
-      isBlock(rightX, innerTopY) ||
-      isOutOfWorld(rightX, topY)
-    ) {
+    const rightHit =
+      isBlock(nextRightX, innerBottomY) ||
+      isBlock(nextRightX, innerTopY) ||
+      isOutOfWorldRight(nextRightX);
+    // 左も同様
+    const leftHit =
+      isBlock(nextLeftX, innerBottomY) ||
+      isBlock(nextLeftX, innerTopY) ||
+      isOutOfWorldLeft(nextLeftX);
+    if (leftHit && rightHit) {
+      // todo: この場合どうするべき?
+      this.vx = 0;
+    } else if (rightHit) {
       this.x =
         (Math.floor(nextX + c.playerWidth / 2) - c.playerWidth / 2) *
         cx.blockSize;
       this.vx = 0;
-    }
-    // プレイヤーの左側がブロック
-    if (
-      isBlock(leftX, innerBottomY) ||
-      isBlock(leftX, innerTopY) ||
-      isOutOfWorld(leftX, topY)
-    ) {
+    } else if (leftHit) {
       this.x =
         (Math.ceil(nextX - c.playerWidth / 2) + c.playerWidth / 2) *
         cx.blockSize;
@@ -207,13 +206,16 @@ export class Player extends Sprite {
     // Todo: 本当はブロックの移動状況含むステージの状況すべてをリセットすべき
     // Todo: ステージ個別に用意される初期座標に移動させる
     // Todo: 直接移動させるのではなく、ゲームオーバー処理を切り分ける
-    if (isOutOfWorld(nextX, topY)) {
+    if (isOutOfWorldBottom(innerTopY)) {
       this.x = 2 * cx.blockSize;
       this.y = 3 * cx.blockSize;
+      this.vx = 0;
+      this.vy = 0;
     }
 
-    // if (bunny.x >= app.screen.width / 2 + 200) {
-    //   app.stage.removeChild(bunny);
-    // }
+    // 当たり判定結果を反映する
+    this.x += this.vx * ticker.deltaTime;
+    this.y += this.vy * ticker.deltaTime;
+    this.vy += c.gravity * cx.blockSize * ticker.deltaTime;
   }
 }
