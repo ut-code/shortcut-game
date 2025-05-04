04b11b2286b39d165b31d3b2d0972ef47677dc21
aster <137767097+aster-void@users.noreply.github.com>
2025-05-04 18:04:28 +0900

    fix build

--- routes/game/+page.svelte
+++ routes/game/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+import { browser } from "$app/environment";
 import { page } from "$app/state";
 import GameLoader from "@/components/GameLoader.svelte";
 import { stages } from "@/stages";
 
 const stageDefinition = $derived(
-  stages.get(page.url.searchParams.get("stage") ?? ""),
+  browser ? stages.get(page.url.searchParams.get("stage") ?? "") : undefined,
 );
 </script>
 
