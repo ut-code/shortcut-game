f2172c8240f4443517bcbcb51d07e11dcff18505
nakomochi <nakomochi13@gmail.com>
2025-05-11 17:27:10 +0900

    stage6を追加

--- routes/+page.svelte
+++ routes/+page.svelte
@@ -8,3 +8,4 @@
 <a href="/game?stage=3">Stage 3</a>
 <a href="/game?stage=4">Stage 4</a>
 <a href="/game?stage=5">Stage 5</a>
+<a href="/game?stage=6">Stage 6</a>
--- src/stages.ts
+++ src/stages.ts
@@ -152,4 +152,76 @@ export const stages = new Map<string, StageDefinition>([
       ],
     },
   ],
+  [
+    "6",
+    {
+      stage: [
+        "bbbbbbbbbbbbbbbbbbbbbbbbb",
+        ".........................",
+        ".........................",
+        "...............wbbbbbbbbb",
+        "m...............bbbbbbbbb",
+        "bb..............w.....m..",
+        "bb..............b....mm..",
+        "bb.w....m..s...bb...mmm..",
+        "bbbbbbbbbbbSbbbbbbbbbbbbb",
+      ],
+      initialPlayerX: 5,
+      initialPlayerY: 8,
+      blockGroups: [
+        {
+          x: 20,
+          y: 7,
+          objectId: "1",
+        },
+        {
+          x: 21,
+          y: 7,
+          objectId: "1",
+        },
+        {
+          x: 21,
+          y: 6,
+          objectId: "1",
+        },
+        {
+          x: 22,
+          y: 5,
+          objectId: "1",
+        },
+        {
+          x: 22,
+          y: 6,
+          objectId: "1",
+        },
+        {
+          x: 22,
+          y: 7,
+          objectId: "1",
+        },
+      ],
+      switchGroups: [
+        {
+          x: 3,
+          y: 7,
+          switchId: "1",
+        },
+        {
+          x: 11,
+          y: 7,
+          switchId: "1",
+        },
+        {
+          x: 15,
+          y: 3,
+          switchId: "1",
+        },
+        {
+          x: 16,
+          y: 5,
+          switchId: "1",
+        },
+      ],
+    },
+  ],
 ]);
