237f8bbee2a4aa28f03dd8cd798b39ddb29cca96
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-05-24 03:19:00 +0900

    start → start from 1-1

--- routes/+page.svelte
+++ routes/+page.svelte
@@ -53,7 +53,7 @@ $effect(() => {
     <div class="flex flex-col gap-5 mt-30 m-10 p-5 bg-yellow-400/85 rounded-lg">
       <h1 class="text-center mt-6 mb-2">Shortcut Game</h1>
       <!--TODO: 題名-->
-      <a class="btn modal-btn text-2xl" href="/game?stage=1-1" bind:this={btn1} tabindex="1"> Start </a>
+      <a class="btn modal-btn text-2xl" href="/game?stage=1-1" bind:this={btn1} tabindex="1"> Start from 1-1 </a>
       <a class="btn modal-btn text-2xl" href="/stage-select" bind:this={btn2} tabindex="2"> Stage Select </a>
       <p class="bg-white/90 p-2 rounded-lg">
         Move: &#x21E6; &#x21E8; or A D<br />
