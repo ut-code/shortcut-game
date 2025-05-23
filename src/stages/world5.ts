1920506ea2591a9edbcb1366b06b2ae0bec28a36
nakomochi <nakomochi13@gmail.com>
2025-05-23 23:05:25 +0900

    stage5-1,5-2追加

--- src/stages/world5.ts
+++ src/stages/world5.ts
@@ -1,38 +1,113 @@
+import { stagePreprocess } from "@/stage-preprocessor.ts";
 import { usages } from "./_proto.ts";
 import type { StageDefinition } from "./type.ts";
 
 export namespace world5 {
-  export const stage1: StageDefinition = {
+  export const stage1: StageDefinition = stagePreprocess({
     stage: [
-      "bbbbbbbbbbbbbbbbbbbbb",
-      "bbbbbbbbbbb..........",
-      "bbbbbbbbbbb..........",
-      "bbbbbbbbbbz..........",
-      ".....................",
-      ".....................",
-      ".f...................",
-      ".m.................g.",
-      "bbbbb^^^^^^......bbbb",
+      "bbbbbbbbbbbbbbbbbbbbbbbb",
+      "....w...................",
+      "....w...................",
+      ".g..w...................",
+      "bbbbbbb.................",
+      ".................bb.....",
+      "....f............bbbbb..",
+      "bbbbbbb.bbwwwwwb.bbm....",
+      "bbbbbbb.bbm..m.b.......m",
+      "bbbbbbbsbbmm.mmb.....smm",
+      "bbbbbbbSbbbbbbbb^bbbbSbb",
     ],
-    initialPlayerX: 4,
-    initialPlayerY: 7,
-    inventoryIsInfinite: true,
-    usage: usages.allInf,
-    blockGroups: [],
-    switchGroups: [],
-    laserDirections: [{ x: 10, y: 3, direction: "down" }],
-  };
-
-  export const stage2: StageDefinition = {
+    overlay: [
+      "bbbbbbbbbbbbbbbbbbbbbbbb",
+      "....2...................",
+      "....2...................",
+      ".g..2...................",
+      "bbbbbbb.................",
+      ".................bb.....",
+      "..S.f............bbbbb..",
+      "bbbbbbb.bb11111b.bbm....",
+      "bbbbbbb.bb5..3.b.......4",
+      "bbbbbbb1bb55.33b.....244",
+      "bbbbbbb-bbbbbbbb^bbbb-bb",
+    ],
+    inventoryIsInfinite: false,
+    usage: {
+      copy: 0,
+      cut: Number.POSITIVE_INFINITY,
+      paste: Number.POSITIVE_INFINITY,
+    },
+  });
+  export const stage2: StageDefinition = stagePreprocess({
     stage: [
       "bbbbbbbbbbbbbbbbbbbbb",
-      "..........g..........",
-      ".........bbb.........",
-      "....f...........z....",
-      "...bbbbb^^^^^bbbbb...",
-      "..b.......w.......b..",
-      ".m........w........m.",
-      "mm...s....w....f...mm",
+      "...........f.........",
+      "...........fmm......g",
+      "..........bbbb.....bb",
+      ".f...................",
+      "bbbbbb.m.............",
+      "bbbbb..m.............",
+      "bbbbb.mm.............",
+      "bbbbb.^^^^^^^^^^^^^^^",
+    ],
+    overlay: [
+      "bbbbbbbbbbbbbbbbbbbbb",
+      "...........f.........",
+      "...........f22......g",
+      "..........bbbb.....bb",
+      ".f.S.................",
+      "bbbbbb.1.............",
+      "bbbbb..1.............",
+      "bbbbb.11.............",
+      "bbbbb.^^^^^^bbbbbbbbb",
+    ],
+    inventoryIsInfinite: false,
+    usage: {
+      copy: 0,
+      cut: Number.POSITIVE_INFINITY,
+      paste: Number.POSITIVE_INFINITY,
+    },
+  });
+  export const stage3: StageDefinition = stagePreprocess({
+    stage: [
+      "bbbbbbbbbbbbbbbbbbbbb",
+      "...........f.........",
+      "...........fmm......g",
+      "..........bbbb.....bb",
+      ".f...................",
+      "bbbbbb.m.............",
+      "bbbbb..m.............",
+      "bbbbb.mm.............",
+      "bbbbb.^^^^^^^^^^^^^^^",
+    ],
+    overlay: [
+      "bbbbbbbbbbbbbbbbbbbbb",
+      "...........f.........",
+      "...........f22......g",
+      "..........bbbb.....bb",
+      ".f.S.................",
+      "bbbbbb.1.............",
+      "bbbbb..1.............",
+      "bbbbb.11.............",
+      "bbbbb.^^^^^^bbbbbbbbb",
+    ],
+    inventoryIsInfinite: false,
+    usage: {
+      copy: 0,
+      cut: Number.POSITIVE_INFINITY,
+      paste: Number.POSITIVE_INFINITY,
+    },
+  });
+
+  export const stage4: StageDefinition = {
+    stage: [
+      "bbbbbbbbbbbbbbbbbbbbb",
+      ".....................",
+      ".....................",
+      ".....................",
+      ".....................",
+      ".....................",
+      ".....................",
+      ".....................",
       "bbbbbSbbbbbbbbbbbbbbb",
     ],
     initialPlayerX: 4,
